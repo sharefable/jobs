1d5f0175c980f65074334a312fccb0b3d6b2e0f5
akash <akash.goswami@unacademy.com>
2023-06-18 20:30:03 +0000

    Add aws dependency

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 
 const PORT = 8081;
 
+const INFO = {
+  timeInSecSinceLastPoll: 0,
+};
+
 const app: Express = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,6 +14,10 @@ app.use(bodyParser.json());
 
 app.get('/health', (req: Request, res: Response) => {
   res.json({ status: 'up' });
+});
+
+app.get('/info', (req: Request, res: Response) => {
+  res.json({ ...INFO });
 });
 
 app.listen(PORT, async () => {
