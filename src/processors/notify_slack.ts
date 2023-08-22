a7faf38d65d967390702420d9978084374136b5b
akash <akash.goswami@unacademy.com>
2023-08-22 13:22:09 +0000

    dynamic params

--- src/processors/notify_slack.ts
+++ src/processors/notify_slack.ts
@@ -5,20 +5,34 @@ import { NfEvents, ReqNfHook } from 'api-contract';
 
 const slackWebhookUrl = 'https://hooks.slack.com/services/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';
 
+const PAYLOAD_PREFIX = 'payload_';
+function getPayloadProps(props: Record<string, string>): string {
+  // const maps: Record<string, string> = {};
+  let payloadAsText = '';
+  for (const [key, value] of Object.entries(props)) {
+    if (key.toLowerCase().startsWith(PAYLOAD_PREFIX)) {
+      const newKey = key.substring(PAYLOAD_PREFIX.length, key.length);
+      payloadAsText += '\n'+`${newKey}: ${value}`;
+    }
+  }
+  return payloadAsText;
+}
+
 type IProps = ReqNfHook & TMsgAttrs;
 export const processEventsToNotify = async (utProps: TMsgAttrs) => {
   const props = utProps as IProps;
   try {
+    const payloadVarStr = getPayloadProps(props as Record<string,string>);
     let text = '';
     switch (props.eventName) {
       case NfEvents.NEW_USER_SIGNUP: {
-        text = `\`\`\`\nevent_name: ${props.eventName}\nemail_id: ${props.payload_emailId}\norg_status: ${props.payload_orgStatus}\nenv: ${process.env.APP_ENV}\n\`\`\``;
+        text = `\`\`\`\nevent_name: ${props.eventName}${payloadVarStr}\nenv: ${process.env.APP_ENV}\n\`\`\``;
         await notifySlack(slackWebhookUrl, text);
         break;
       } 
 
       case NfEvents.EBOOK_DOWNLOAD: {
-        text = `\`\`\`\nevent_name: ${props.eventName}\nfirst_name: ${props.payload_firstName}\nemail_id: ${props.payload_email}\nenv: ${process.env.APP_ENV}\n\`\`\``;
+        text = `\`\`\`\nevent_name: ${props.eventName}${payloadVarStr}\nenv: ${process.env.APP_ENV}\n\`\`\``;
         await notifySlack(slackWebhookUrl, text);
         break;
       }
