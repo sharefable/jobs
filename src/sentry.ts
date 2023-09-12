29b91f82a9f127109584019c1afb84a2ffd51111
Fahumitha <fahumitha@sharefable.com>
2023-09-12 17:57:44 +0530

    Modified incorrect env vaiable name

--- src/sentry.ts
+++ src/sentry.ts
@@ -5,7 +5,7 @@ const DSN_KEY_JOBS = 'https://b60b8299d85ce00707286d674174523d@o4505113177620480
 export const sentryInitialize = () => {
   init({
     dsn: DSN_KEY_JOBS,
-    environment: process.env.JOB_ENV,
+    environment: process.env.APP_ENV,
     tracesSampleRate: 1.0,
   });
 };
