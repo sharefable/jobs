6313758088f607fda7c6807761b71e95cf553ee5
Fahumitha <fahumitha@sharefable.com>
2024-03-05 17:16:26 +0530

    update api contract

--- src/api-contract.ts
+++ src/api-contract.ts
@@ -1,12 +1,17 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.35.1025 on 2023-08-21 14:04:21.
+// Generated using typescript-generator version 2.35.1025 on 2024-03-05 16:47:06.
 
 export interface ApiResp<T> {
     status: ResponseStatus;
     data: T;
     errStr: string;
     errCode: ErrorCode;
+}
+
+export interface ButtonClicks {
+    btnId: string;
+    totalClicks: number;
 }
 
 export interface EntityHoldingInfoBase extends Serializable {
@@ -32,11 +37,65 @@ export interface MediaTypeEntityHolding extends EntityHoldingInfoBase {
     deletable: boolean;
 }
 
+export interface PaymentTerms {
+}
+
+export interface ReqTourPropUpdate {
+    tourRid: string;
+    inProgress?: boolean;
+}
+
+export interface ScreenAssets {
+    thumbnail: string;
+    name: string;
+    url: string;
+    icon: string;
+}
+
+export interface TotalVisitorsByYmd {
+    totalViews: number;
+    ymd: string;
+}
+
+export interface TourAnnViewsWithPercentile {
+    annId: string;
+    totalViews: number;
+    p1: number;
+    p5: number;
+    p10: number;
+    p25: number;
+    p50: number;
+    p75: number;
+    p90: number;
+    p95: number;
+    p99: number;
+}
+
+export interface TourAnnWithViews {
+    annId: string;
+    totalViews: number;
+    p50: number;
+    p75: number;
+    p95: number;
+}
+
+export interface TourManifest {
+    version: number;
+    name: string;
+    url: string;
+    screenAssets: ScreenAssets[];
+}
+
 export interface VideoTranscodingJobInfo extends JobProcessingInfo {
     sourceFilePath: string;
     processedFilePath: string;
     sub: VideoProcessingSub;
     meta: string;
+}
+
+export interface ReqActivateOrDeactivateUser {
+    userId: number;
+    shouldActivate: boolean;
 }
 
 export interface ReqCopyScreen {
@@ -87,6 +146,7 @@ export interface ReqNfHook {
 export interface ReqProxyAsset {
     origin: string;
     clientInfo: string;
+    body?: boolean;
 }
 
 export interface ReqRecordEdit {
@@ -96,6 +156,7 @@ export interface ReqRecordEdit {
 
 export interface ReqRenameGeneric {
     newName: string;
+    description?: string;
     rid: string;
 }
 
@@ -104,8 +165,17 @@ export interface ReqScreenTour {
     tourRid: string;
 }
 
+export interface ReqSubscriptionInfo {
+    pricingPlan: Plan;
+    pricingInterval: Interval;
+}
+
 export interface ReqThumbnailCreation {
     screenRid: string;
+}
+
+export interface ReqTourRid {
+    tourRid: string;
 }
 
 export interface ReqUpdateScreenProperty {
@@ -123,9 +193,21 @@ export interface RespCommonConfig extends ResponseBase {
     commonAssetPath: string;
     screenAssetPath: string;
     tourAssetPath: string;
+    pubTourAssetPath: string;
     dataFileName: string;
+    loaderFileName: string;
     editFileName: string;
+    pubDataFileName: string;
+    pubLoaderFileName: string;
+    pubEditFileName: string;
+    pubTourEntityFileName: string;
+    manifestFileName: string;
     latestSchemaVersion: SchemaVersion;
+}
+
+export interface RespConversion {
+    tourId: number;
+    buttonsWithTotalClicks: ButtonClicks[];
 }
 
 export interface RespHealth extends ResponseBase {
@@ -149,6 +231,7 @@ export interface RespOrg extends ResponseBase {
 
 export interface RespProxyAsset extends ResponseBase {
     proxyUri: string;
+    content?: string;
 }
 
 export interface RespScreen extends ResponseBase {
@@ -166,13 +249,39 @@ export interface RespScreen extends ResponseBase {
     uploadUrl?: string;
 }
 
+export interface RespSubscription extends ResponseBase {
+    paymentPlan: Plan;
+    paymentInterval: Interval;
+    status: Status;
+    trialStartedOn: Date;
+    trialEndsOn: Date;
+}
+
 export interface RespTour extends ResponseBase {
     id: number;
     rid: string;
     assetPrefixHash: string;
     displayName: string;
     description: string;
+    lastPublishedDate: Date;
+    onboarding: boolean;
+    inProgress: boolean;
     createdBy: RespUser;
+}
+
+export interface RespTourAnnViews {
+    tourId: number;
+    tourAnnWithViews: TourAnnWithViews[];
+}
+
+export interface RespTourAnnWithPercentile {
+    tourAnnInfo: TourAnnViewsWithPercentile[];
+}
+
+export interface RespTourView {
+    tourId: number;
+    totalViews: SumViews;
+    totalVisitorsByYmd: TotalVisitorsByYmd[];
 }
 
 export interface RespTourWithScreens extends RespTour {
@@ -193,8 +302,8 @@ export interface RespUser extends ResponseBase {
     email: string;
     avatar: string;
     personalEmail: boolean;
-    belongsToOrg: number;
     orgAssociation: UserOrgAssociation;
+    active: boolean;
 }
 
 export interface Serializable {
@@ -208,6 +317,11 @@ export interface ResponseBase {
     updatedAt: Date;
 }
 
+export interface SumViews {
+    viewsAll: number;
+    viewsUnique: number;
+}
+
 export const enum EntityType {
     Screen = 0,
     Tour = 1,
@@ -216,6 +330,7 @@ export const enum EntityType {
 export const enum EntryDurationType {
     CURRENT = "CURRENT",
     DAILY = "DAILY",
+    LIFETIME = "LIFETIME",
 }
 
 export const enum JobProcessingStatus {
@@ -228,6 +343,7 @@ export const enum JobProcessingStatus {
 export const enum JobType {
     TRANSCODE_VIDEO = "TRANSCODE_VIDEO",
     RESIZE_IMG = "RESIZE_IMG",
+    DELETE_ASSET = "DELETE_ASSET",
     REFRESH_CRAWLER = "REFRESH_CRAWLER",
     REFRESH_CRAWLER_FOR_ANN_USER_ASSIGN = "REFRESH_CRAWLER_FOR_ANN_USER_ASSIGN",
     REFRESH_TOUR_ANN_CLICK = "REFRESH_TOUR_ANN_CLICK",
@@ -273,6 +389,26 @@ export const enum ErrorCode {
 export const enum NfEvents {
     NEW_USER_SIGNUP = "NEW_USER_SIGNUP",
     EBOOK_DOWNLOAD = "EBOOK_DOWNLOAD",
+}
+
+export const enum Plan {
+    STARTUP = "STARTUP",
+    BUSINESS = "BUSINESS",
+}
+
+export const enum Interval {
+    MONTHLY = "MONTHLY",
+    YEARLY = "YEARLY",
+}
+
+export const enum Status {
+    FUTURE = "FUTURE",
+    IN_TRIAL = "IN_TRIAL",
+    ACTIVE = "ACTIVE",
+    NON_RENEWING = "NON_RENEWING",
+    PAUSED = "PAUSED",
+    CANCELLED = "CANCELLED",
+    _UNKNOWN = "_UNKNOWN",
 }
 
 export const enum UserOrgAssociation {
