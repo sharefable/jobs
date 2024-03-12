fe0517e40f696ff61d6a11dfafbe7966c95d6137
Fahumitha <fahumitha@sharefable.com>
2024-03-12 10:36:50 +0530

    query to select only distinct rows

--- src/jobs/lead_activity/queries.ts
+++ src/jobs/lead_activity/queries.ts
@@ -2,7 +2,7 @@ import { executeQuery } from '../mysql';
 import { AnalyticsUserAidMappingEntity, TableName } from '../../types';
 
 export const getTourLeadsForYmd = async (ymd: string) => {
-  const query = `SELECT * From ${TableName.AnalyticsUserAidMapping} WHERE date_ymd <= ${ymd};`;
+  const query = `SELECT DISTINCT aid, tour_id From ${TableName.AnalyticsUserAidMapping} WHERE date_ymd <= ${ymd};`;
   const tourLeads: AnalyticsUserAidMappingEntity[] = await executeQuery(query);
   return tourLeads;
 };
