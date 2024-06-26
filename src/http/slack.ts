f5807e11f07054b59b668cdbd9ae1af02d8d1b35
akash <akash@sharefable.com>
2024-06-26 18:18:52 +0000

    Add command of pango

--- src/http/slack.ts
+++ src/http/slack.ts
@@ -1,8 +1,11 @@
 import {Express, Request, Response} from 'express';
-import {getLiveAndPublishedTourAssetsByRid, getTourByRid} from '../api';
+import {getLiveAndPublishedTourAssetsByRid } from '../api';
 import {RespTour} from '../api-contract';
 import * as log from '../log';
 import * as Sentry from '@sentry/node';
+import { executeQuery } from '../jobs/mysql';
+
+const Table = require('ascii-table3');
 
 function getMsgBlockFor(route: 'public' | 'private', data: {
   host: string,
@@ -182,6 +185,141 @@ async function unFurlSlackContent(urls: URL[], opts: {
 export default function addHttpListeners(app: Express) {
   app.post('/v1/slack/handle', (req: Request, res: Response) => {
     res.type('text/plain').send('ok');
+  });
+
+  app.post('/v1/slack/pango/user-details', async (req: Request, res: Response) => {
+    const payloadStr = req.body.text;
+    if (!payloadStr) {
+      res.type('text/plain').send('Empty not allowd');
+      return;
+    }
+
+    // format: email=hello@mail.com
+    const nPayloadStr = payloadStr.trim();
+    const [key, value] = nPayloadStr.split('=').map((w: string) => w.trim());
+    if (key !== 'email') {
+      res.type('text/plain').send('Invalid');
+      return;
+    }
+
+    const result: any[] = await executeQuery(`SELECT * from user RIGHT OUTER JOIN user_org_join ON user.id = user_org_join.user_id
+    INNER JOIN org ON user_org_join.org_id = org.id
+    INNER JOIN subscriptions ON org.id = subscriptions.org_id
+where  user.email like '${value}'`);
+
+    if (result.length == 0) {
+      res.type('text/plain').send(`No data found for user ${value}`);
+      return;
+    }
+
+    let tableStr = `Data for user: ${value}\n\n`;
+    let i = 0;
+    for (const r of result) {
+      const t = new Table.AsciiTable3(`Entry ${++i}`).setAligns([Table.AlignmentEnum.LEFT, Table.AlignmentEnum.LEFT]);
+      Object.entries(r).forEach(([k, v]) => {
+        if (k === 'auth_id' || k === 'avatar') return;
+        t.addRow(k, v == undefined || v === null ? '<null>' : v);
+      });
+      tableStr += `${t.toString()}\n\n`;
+    }
+
+    res.type('text/plain').send(`
+\`\`\`
+${tableStr}
+\`\`\` `);
+  });
+
+  app.post('/v1/slack/pango/update-feature', async (req: Request, res: Response) => {
+    try {
+      const payloadStr = req.body.text;
+      if (!payloadStr) {
+        res.type('text/plain').send('Empty not allowd');
+        return;
+      }
+
+      const nPayloadStr = payloadStr.trim();
+      const lines = nPayloadStr.split(/\n+/);
+      const [orgIdStr, ...featureGateOverrideArr] = lines;
+      const featureOverride = featureGateOverrideArr.join('\n');
+
+      const [key, orgId] = orgIdStr.split('=').map((w: string) => w.trim()); 
+      if (key.toLowerCase() !== 'orgid') {
+        res.type('text/plain').send('Invalid');
+        return;
+      }
+      const nOrgId = +(orgId.trim());
+      if (Number.isNaN(nOrgId)) {
+        res.type('text/plain').send('Invalid orgId. It could only be a number');
+        return;
+      }
+
+      const orgs = await executeQuery(`select * from org where id = ${nOrgId}`);
+      const org: any = orgs[0];
+      if (!org) {
+        res.type('text/plain').send(`Org with id ${nOrgId} not found`);
+        return;
+      }
+
+      if (!featureOverride) {
+        res.type('text/plain').send('Invalid feature override. It should be in ```json``` format');
+        return;
+      }
+
+      let nFeatureOverride = featureOverride.trim();
+      if (!(nFeatureOverride.startsWith('```') && nFeatureOverride.endsWith('```'))) {
+        res.type('text/plain').send('Invalid feature override. It should be in ```json``` format');
+        return;
+      }
+
+      nFeatureOverride = nFeatureOverride.substring(3, nFeatureOverride.length - 3);
+      try {
+        let info;
+        if (!info) {
+          info = {
+            useCases: ['marketing'],
+            othersText: '',
+          };
+        } else info = JSON.parse(org.info);
+
+        if (nFeatureOverride === 'null') {
+          delete info.bet;
+        } else {
+          const featureOverrideJson = JSON.parse(nFeatureOverride);
+          if ('useCases' in featureOverrideJson
+            || 'othersText' in featureOverrideJson
+            || 'bet' in featureOverrideJson
+            || 'featureGateOverride' in featureOverrideJson) {
+            res.type('text/plain').send('Feature override should not have useCases or othersText or bet or featureGateOverride');
+            return;
+          }
+
+          info = {
+            ...info,
+            bet: {
+              featureGateOverride: featureOverrideJson,
+            },
+          };
+        }
+
+        await executeQuery(`update org set info = '${JSON.stringify(info)}' where id = ${nOrgId}`);
+        res.type('text/plain').send(`Updated featuregatematrix for orgId=${nOrgId}.
+From:
+\`\`\`
+${JSON.stringify(JSON.parse(org.info), null, 2)}
+\`\`\`
+To:
+\`\`\`
+${JSON.stringify(info, null, 2)}
+\`\`\` `);
+      } catch (e) {
+        // eslint-disable-next-line prefer-template
+        res.type('text/plain').send('Invalid feature override. It should be in ```json``` format. Error: ' + (e as Error).message);
+        return;
+      }
+    } catch(e) {
+      // eslint-disable-next-line prefer-template
+      res.type('text/plain').send('Error while updating feature override' + (e as Error).message);
+    }
   });
 
   app.post('/v1/slack/url_verification', (req: Request, res: Response) => {
